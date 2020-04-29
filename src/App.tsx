501e9ccd269c1ea906f3a8261de4b35f465255ff
Amigoni <paulo.amigoni@gmail.com>
2020-04-29 15:41:00 -0300

    estilos login

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import SignIn from './pages/Signin'
+import GlobalStyle from '../src/styles/global';
+
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStyle />
+  </>
+);
+
+export default App;
